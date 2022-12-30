0edb1ccd5eed28dfc70f070404bf1f826022457c
bigbrainbrian7 <brianismlg222@gmail.com>
2022-12-30 15:06:10 -0800

    Fixed bugs

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from website import create_app
+import socket
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='host machine ip', debug=True)
+    gw = os.popen("ip -4 route show default").read().split()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((gw[2], 0))
+    ipaddr = s.getsockname()[0]
+    app.run(host=ipaddr, debug=True)
