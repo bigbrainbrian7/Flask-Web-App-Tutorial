864d6c5c23301ad593945a5a6e2ff47498eaa756
bigbrainbrian7 <87093773+bigbrainbrian7@users.noreply.github.com>
2023-01-02 17:23:40 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    #w = os.popen("ip -4 route show default").read().split()
-    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #s.connect((gw[2], 0))
-    #ipaddr = s.getsockname()[0]
-    ipaddr = os.getenv('ipaddr')
+    w = os.popen("ip -4 route show default").read().split()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((gw[2], 0))
+    ipaddr = s.getsockname()[0]
     app.run(host=ipaddr, debug=True)
     print(ipaddr)
